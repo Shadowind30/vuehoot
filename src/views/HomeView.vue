a9fe72713919d28821ae890ab719ec8202b8ea7f
Carlos Sánchez <casw30rb@gmail.com>
2025-02-12 03:04:12 -0400

    feat: input and button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,20 @@ const text = 'Hello, Vue 3 + Vite!'
 </script>
 
 <template>
-  <main>
-    {{ text }}
-  </main>
-
-  <v-btn color="primary">Hello, world!</v-btn>
+  <v-container>
+    <v-form>
+      <v-row align="center">
+        <v-col cols="6">
+          <v-text-field label="Nickname"></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row align="center">
+        <v-col cols="5">
+          <v-btn color="primary">Play</v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
 </template>
 
 <style scoped>
